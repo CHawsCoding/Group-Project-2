faa6f79dd783fd0dc183c48c6a94b49392f69bac
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-07-27 21:07:25 -0600

    added models with all needed tables as well as associations in index. Also added package.json info

---
+++ models/song.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Song extends Model {}
+
+Song.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    artist: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'song',
+  }
+);
+
+module.exports = Song;
