6a15687d052ee2a384e0387e7c0d57e794867876
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-07-27 21:02:03 -0600

    boilerplate server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
 const sequelize = require("./config/connections");
+const routes = require('./controllers');
 
 const app = express();
 
@@ -12,11 +13,17 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
+app.use(routes);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine(
   "handlebars",
   require("express-handlebars")({ defaultLayout: "homepage" })
 );
 app.set("view engine", "handlebars");
+
 
 // Handle invalid routes
 app.use((req, res) => {
