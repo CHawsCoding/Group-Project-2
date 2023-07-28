41bebee846d0a3bb437266510e33cce981982197
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-07-27 20:51:43 -0600

    boilerplate server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+// Import required modules
+const express = require("express");
+const path = require("path");
+const passport = require("passport");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const sequelize = require("./config/connections");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.engine(
+  "handlebars",
+  require("express-handlebars")({ defaultLayout: "homepage" })
+);
+app.set("view engine", "handlebars");
+
+// Handle invalid routes
+app.use((req, res) => {
+    res.status(404).send('404 - Not Found');
+  });
+
+async function startServer() {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+  }
+}
+
+startServer();
