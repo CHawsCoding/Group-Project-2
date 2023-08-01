7dd9956f6621578143369c7322989389679030c2
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-07-31 19:19:28 -0600

    syncing so I can pull main

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const passport = require("passport");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
 const sequelize = require("./config/connections");
-const routes = require('./controllers');
+
 
 const app = express();
 
@@ -13,18 +13,21 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.use(routes);
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine(
   "handlebars",
-  require("express-handlebars")({ defaultLayout: "homepage" })
+  require("express-handlebars")({ defaultLayout: "main" })
 );
 app.set("view engine", "handlebars");
 
-
+app.get('/', (req, res) => {
+    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    res.render('main', {layout : 'login'});
+    });
 // Handle invalid routes
 app.use((req, res) => {
     res.status(404).send('404 - Not Found');
@@ -34,8 +37,8 @@ async function startServer() {
   try {
     await sequelize.authenticate();
     console.log("Database connection has been established successfully.");
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   } catch (err) {
     console.error("Unable to connect to the database:", err);
