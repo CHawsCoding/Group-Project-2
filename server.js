dd03a4ce7a399266499fa8f318035ba1d1c2df88
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-08-02 19:33:34 -0600

    added code to passport.js - and reaaranged the server.js to account for the passport middleware - also added a block of code to generate SEssion secret.

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 // Import required modules
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const passport = require("passport");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
 const sequelize = require("./config/connection.js");
-const routes = require('./controllers/index-routes')
+const routes = require('./controllers/index-routes');
+const uuid = require("uuid");
+const sessionSecret = uuid.v4();
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+if (!process.env.SESSION_SECRET) {
+  const sessionSecret = uuid.v4();
+  process.env.SESSION_SECRET = sessionSecret;
+}
+
+require("./config/passport")(passport);
+
+app.use(
+  session({
+    secret: "your_session_secret_here",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.json());
-
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.engine(
   "handlebars",
