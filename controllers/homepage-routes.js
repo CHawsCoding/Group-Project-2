4c2327b91f387d327ece2b9fd59aacefa0544750
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-08-02 20:18:24 -0600

    added homepage routes to show reviews

---
+++ controllers/homepage-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { Review, User, Song, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all reviews and JOIN with user data
+      const reviewData = await Review.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const reviews = reviewData.map((review) => review.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        reviews, 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
--- controllers/index-routes.js
+++ controllers/index-routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const homepageRoutes = require('./homepage-routes');
 
 
 // router.use('/comments', commentRoutes);
 // router.use('/reviews', reviewRoutes);
 // router.use('/songs', songRoutes);
+router.use('/', homepageRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
