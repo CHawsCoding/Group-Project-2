7b05cdd5941b2fafed3314ec6c2f1327fd5a814a
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-08-06 13:45:05 -0600

    edited review-routes file - needed to updated existing functions to use passport to ensure the user is authenitcated to post/put/delete reviews - also added a get route to get all reviews - just incase we want to use that for anything else

--- controllers/review-routes.js
+++ controllers/review-routes.js
@@ -1,37 +1,55 @@
-const router = require('express').Router();
-const { review } = require('../../models');
+const router = require("express").Router();
+const { Review, User, Song } = require("../models");
 
-router.post('/', async (req, res) => {
-    try {
-      const newReview = await Review.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newReview);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-  
-  router.delete('/:id', async (req, res) => {
-      try {
-        const ReviewData = await Review.destroy({
-          where: {
-            id: req.params.id,
-            user_id: req.session.user_id,
-          },
-        });
-    
-        if (!ReviewData) {
-          res.status(404).json({ message: 'No Review found with this id!' });
-          return;
-        }
-    
-        res.status(200).json(projectData);
-      } catch (err) {
-        res.status(500).json(err);
-      }
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login"); // Redirects user to the login page if not authenticated
+};
+
+// Get all reviews
+router.get("/reviews", async (req, res) => {
+  try {
+    const reviewData = await Review.findAll({
+      include: [{ model: User }, { model: Song }],
     });
-  
-    module.exports = router;
+    res.status(200).json(reviewData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/review", ensureAuthenticated, async (req, res) => {
+  try {
+    const newReview = await Review.create({
+      ...req.body,
+      user_id: req.user.id, // Add user ID from session
+    });
+    res.status(200).json(newReview);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/review/:id", ensureAuthenticated, async (req, res) => {
+  try {
+    const updatedReview = await Review.update(req.body, {
+      where: { id: req.params.id, user_id: req.user.id }, // Add user ID check
+    });
+    res.status(200).json(updatedReview);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/review/:id", ensureAuthenticated, async (req, res) => {
+  try {
+    const deletedReview = await Review.destroy({
+      where: { id: req.params.id, user_id: req.user.id }, // Add user ID check
+    });
+    res.status(200).json(deletedReview);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
