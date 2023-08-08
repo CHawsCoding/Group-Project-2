c7f1af49e0e7e4f7819dfc3a33f0939b908c2d8d
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-08-07 18:51:39 -0600

    fixing db

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const passport = require("passport");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
 const sequelize = require("./config/connection.js");
-const routes = require('./controllers/index-routes');
+const routes = require("./controllers/index-routes");
 const uuid = require("uuid");
 const sessionSecret = uuid.v4();
-const userRoutes = require('./controllers/user-routes');
-const helpers = require('./utils/helpers');
-const reviewRoutes = require('./controllers/review-routes.js');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const userRoutes = require("./controllers/user-routes");
+const helpers = require("./utils/helpers");
+const reviewRoutes = require("./controllers/review-routes.js");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 
@@ -27,18 +26,18 @@ if (!process.env.SESSION_SECRET) {
 require("./config/passport")(passport);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {
     maxAge: 30 * 60 * 1000,
     httpOnly: true,
     secure: false,
-    sameSite: 'strict',
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -47,11 +46,11 @@ app.use(passport.initialize());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 app.use(routes);
-app.use('/review', reviewRoutes);
+app.use("/review", reviewRoutes);
 
 app.engine(
   "handlebars",
@@ -59,24 +58,26 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-app.get('/', (req, res) => {
-    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('homepage', {layout : 'main'});
-    });
+app.get("/", (req, res) => {
+  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+  res.render("homepage", { layout: "main" });
+});
 // Handle invalid routes
 app.use((req, res) => {
-    res.status(404).send('404 - Not Found');
-  });
+  res.status(404).send("404 - Not Found");
+});
 
 async function startServer() {
   try {
-    await sequelize.authenticate();
-    console.log("Database connection has been established successfully.");
+    await sequelize.sync({ force: false });
+
+    console.log("All models were synchronized successfully.");
+
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (err) {
-    console.error("Unable to connect to the database:", err);
+    console.error("Unable to synchronize models to the database:", err);
   }
 }
 
